7acaae4bf89909032d4dde69e45d7e588e3d5304
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-05-16 21:40:41 -0600

    w2 team act

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -11,10 +11,17 @@ export function getLocalStorage(key) {
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  let currentData = getLocalStorage(key) || [];
-  currentData.push(data);
-  localStorage.setItem(key, JSON.stringify(currentData));
+  localStorage.setItem(key, JSON.stringify(data));
 }
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
